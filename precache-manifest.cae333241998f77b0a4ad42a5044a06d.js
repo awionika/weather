41c0dda5c65d2410d7495cd658781d13c0a03eeb
awionika <awionika@users.noreply.github.com>
2020-02-20 10:54:42 +0100

    Updates

---
+++ precache-manifest.cae333241998f77b0a4ad42a5044a06d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "678e5d2d281c5d6ef4ab491efba07daf",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "52a761013ae1ce1019c3",
+    "url": "/weather/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "1a70c95c85b54eea365c",
+    "url": "/weather/static/css/main.e25f02f8.chunk.css"
+  },
+  {
+    "revision": "52a761013ae1ce1019c3",
+    "url": "/weather/static/js/2.5cacfdea.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/weather/static/js/2.5cacfdea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a70c95c85b54eea365c",
+    "url": "/weather/static/js/main.6344167f.chunk.js"
+  },
+  {
+    "revision": "c99e76bcf2bdae1a8b8a",
+    "url": "/weather/static/js/runtime-main.953fba7f.js"
+  },
+  {
+    "revision": "3b233dcf71d6528e51101e1ea2136dfa",
+    "url": "/weather/static/media/pogoda.3b233dcf.jpg"
+  }
+]);
